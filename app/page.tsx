9e078bb610a0ba3d7fa56c7332011cdcdd881f26
gokul satya <56232654+gokulsatya@users.noreply.github.com>
2025-01-10 23:50:36 -0500

    commiting chat

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import ChatInterface from '@/components/ChatInterface'
+import ChatInterface from './ChatInterface'
 
 export default function Home() {
- return (
-   <div className="min-h-screen bg-base-200 p-4">
-     <div className="container mx-auto max-w-4xl">
-       <ChatInterface />
-     </div>
-   </div>
- )
+  return (
+    <div className="min-h-screen bg-base-200 p-4">
+      <div className="container mx-auto max-w-4xl">
+        <ChatInterface />
+      </div>
+    </div>
+  )
 }
