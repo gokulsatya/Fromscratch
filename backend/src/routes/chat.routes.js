73842e05ed6bfe1653a48b0b13f48cc61c1f550a
gokul satya <56232654+gokulsatya@users.noreply.github.com>
2025-03-08 23:05:05 -0500

    from scrath

--- backend/src/routes/chat.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controllers/chat.controller');
-
-router.post('/', chatController.createChat);
-router.get('/:id', chatController.getChat);
-router.post('/:id/message', chatController.addMessage);
-
-module.exports = router;
